ca6fb967a093110b03004a7d09eee262597cc024
Vincent Nduri <ndurivo@gmail.com>
2023-03-22 02:16:03 +0300

    Adds default font family and styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
     extend: {},
   },
